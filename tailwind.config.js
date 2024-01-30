1c465d41f64c3ea271b2cf58aaad4bb55212ac0a
fathima-1234 <fathimasuhra19@gmail.com>
2024-01-30 15:59:54 +0530

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,jsx,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "black-ops": ["Black Ops One", "cursive"],
+        Varela: ["Varela Round", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
